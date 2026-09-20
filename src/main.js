91adcf839ae8dfb799aab9c1ab64f12ca225346c
Svitlana Hnylytska <alyate173@gmail.com>
2026-09-20 22:27:04 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,34 +1,28 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
-import { getImagesByQuery } from "./js/pixabay-api";
-import {
-  createGallery,
-  clearGallery,
-  showLoader,
-  hideLoader,
-} from "./js/render-functions";
-
-const form = document.querySelector(".form");
-
-form.addEventListener("submit", event => { event.preventDefault();
-const query = form.elements["search-text"].value.trim();
-if (query === "") { return; }
-clearGallery(); showLoader();
-getImagesByQuery(query) .then(data => { if (data.hits.length === 0) { iziToast.error({ message: "Sorry, there are no images matching your search query. Please try again!", });
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const query = form.elements["search-text"].value.trim();
+  if (query === "") {
     return;
   }
-
-  createGallery(data.hits);
-})
-.catch(error => {
-  iziToast.error({
-    message: "Something went wrong. Please try again later.",
-  });
-
-  console.error(error);
-})
-.finally(() => {
-  hideLoader();
-});
+  clearGallery();
+  showLoader();
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            "Sorry, there are no images matching your search query. Please try again!",
+        });
+        return;
+      }
+      createGallery(data.hits);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: "Something went wrong. Please try again later.",
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
 });
